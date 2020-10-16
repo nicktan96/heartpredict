b50b96d6b1902ffca2af6427b6193cc7021752d9
Nicholas Tan Jian Zhi <58356997+nicktan96@users.noreply.github.com>
2020-10-17 00:51:25 +1100

    Add files via upload

--- ML.py
+++ ML.py
@@ -4,18 +4,23 @@ import pandas as pd
 import pickle
 from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
+from sklearn import preprocessing
 
-dataset = pd.read_csv('heartedit.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
+dataset = pd.read_csv('heart10.csv')
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,-1].values
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size =0.20)
 
-classifier = RandomForestClassifier(max_features = 'log2', n_estimators = 250, criterion = 'entropy', random_state = 10)
+sc = preprocessing.StandardScaler()
+X_train = pd.DataFrame(sc.fit_transform(X_train))
+X_test = pd.DataFrame(sc.transform(X_test))
+
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 250, criterion = 'gini', max_features = 'log2', random_state=0)
 classifier.fit(X_train, y_train)
 
-
-pickle.dump(classifier, open('heart.pkl', 'wb'))
-model=pickle.load(open('heart.pkl', 'rb'))
-print("done")
+y_pred = classifier.predict(X_test)
+pickle.dump(classifier, open('genModel.pkl', 'wb'))
+model=pickle.load(open('genModel.pkl', 'rb'))
